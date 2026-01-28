8622590f9401ab4579f4c0f693aafba13092b2e5
ar928181 <78840144+ar928181@users.noreply.github.com>
2026-01-28 16:58:47 +0500

    Kubernetes and Course completed

--- Section 7 Kubernetes with Microservices using Docker -V3/05.kubernetes/currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
+++ Section 7 Kubernetes with Microservices using Docker -V3/05.kubernetes/currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.in28minutes.microservices.currencyconversionservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Fail.fail;
+
 @SpringBootTest
 class CurrencyConversionServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+        //fail("failes");
 	}
 
 }
